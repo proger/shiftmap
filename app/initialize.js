ac4de4b1af83b8a8521b2459b12a4a499a1f74e3
Vlad Ki <vlad@kirillov.im>
2016-08-03 16:22:59 -0700

    flycheck says i must fix these

--- app/initialize.js
+++ app/initialize.js
@@ -1,7 +1,7 @@
 var jsfeat = require("jsfeat");
 var profiler = require("./profiler.js");
 var compatibility = require("./compatibility.js");
-// var prob = require("./prob.js")
+var prob = require("./prob.js");
 
 /// repopulates RGBA imageData buffer using the grayscale matrix (mutating)
 function matrix2id_gray(matrix, imageData) {
@@ -57,48 +57,52 @@ function getGradientMagnitude(img) {
             var idx = i * gradient.cols + j;
             magnitude.data[idx] = Math.sqrt(
                 gradient.data[2 * idx] * gradient.data[2 * idx] +
-                    gradient.data[2 * idx + 1] * gradient.data[2 * idx + 1])
+                    gradient.data[2 * idx + 1] * gradient.data[2 * idx + 1]);
         }
     }
 
-    return magnitude
+    return magnitude;
 }
 
 function countShiftmapDiscontinuties(shiftmap) {
-    var count = 0
+    var count = 0;
     var dirs = [{x:  0, y: -1}, // above
                 {x:  0, y:  1}, // bellow
                 {x: -1, y:  0}, // left
-                {x:  1, y:  0}] // right
-    var i, j
+                {x:  1, y:  0}]; // right
+    var i, j;
 
     for(i = 0; i < shiftmap.length; i++) {
         for(j = 0; j < shiftmap[i].length; j++) {
             for(d = 0; d < dirs.length; d++){
-               var ii = i + dirs[d].y, jj = j + dirs[d].x
+                var ii = i + dirs[d].y, jj = j + dirs[d].x;
                // Handle top and bottom rows
-               if(ii < 0 || ii === shiftmap.length) continue
+                if(ii < 0 || ii === shiftmap.length) {
+                    continue;
+                }
                 // Handle left and right cols
-               if(jj < 0 || jj === shiftmap[i].length) continue
+                if (jj < 0 || jj === shiftmap[i].length) {
+                    continue;
+                }
 
                if(shiftmap[i][j].x !== shiftmap[ii][jj].x ||
                   shiftmap[i][j].y !== shiftmap[ii][jj].y) {
-                    count++
+                   count++;
                 }
             }
         }
     }
 
-    return count
+    return count;
 }
 
 function applyShiftmap(src, shiftmap, dest) {
-    var i, j
+    var i, j;
     for(i = 0; i < shiftmap.length; i++) {
         for(j = 0; j < shiftmap[i].length; j++) {
-            var dest_idx = 3 * (i * shiftmap[i].length + j)
-            var src_idx = dest_idx + 3 * (shiftmap[i][j].y * shiftmap[i].length + shiftmap[i][j].x)
-            dest.data[dest_idx] = src.data[src_idx]
+            var dest_idx = 3 * (i * shiftmap[i].length + j);
+            var src_idx = dest_idx + 3 * (shiftmap[i][j].y * shiftmap[i].length + shiftmap[i][j].x);
+            dest.data[dest_idx] = src.data[src_idx];
         }
     }
 }
@@ -115,9 +119,9 @@ function tick() {
         [{x: 1, y: 1}, {x: 1, y: 0}, {x: 1, y: 1}],
         [{x: 1, y: 1}, {x: 1, y: 1}, {x: 2, y: 1}],
         [{x: 1, y: 1}, {x: 1, y: 1}, {x: 1, y: 1}]
-    ]
+    ];
 
-    console.log(countShiftmapDiscontinuties(shiftmap))
+    console.log(countShiftmapDiscontinuties(shiftmap));
 
 
     // withCanvasImageData(document.getElementById('resample'), img, function(imageData) {
