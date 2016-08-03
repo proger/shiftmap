3af7e4f8c2e63c9e1c51cc1641bebf3234eb2eba
Vlad Ki <vlad@kirillov.im>
2016-08-02 17:05:47 -0700

    parrots & cleanup

--- app/initialize.js
+++ app/initialize.js
@@ -4,31 +4,18 @@ var compatibility = require("./compatibility.js");
 var prob = require("./prob.js")
 
 var stat = new profiler();
-var canvas,ctx,canvasWidth,canvasHeight;
-
-function demo_app(videoWidth, videoHeight) {
-    canvasWidth  = canvas.width;
-    canvasHeight = canvas.height;
-    ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = "rgb(0,255,0)";
-    ctx.strokeStyle = "rgb(0,255,0)";
-
-    stat.add("grayscale");
-}
+var canvas, ctx, canvasWidth, canvasHeight;
 
 function tick() {
     stat.new_frame();
 
-    var myImage = document.getElementById("penguins");
-
-    ctx.drawImage(myImage, 0, 0);
-    var imageData = ctx.getImageData(0, 0, 640, 480);
+    var img = document.getElementById("sourceImage");
+    ctx.drawImage(img, 0, 0);
+    var imageData = ctx.getImageData(0, 0, img.width, img.height);
 
     stat.start("grayscale");
-    var gray_img = new jsfeat.matrix_t(640, 480, jsfeat.U8_t | jsfeat.C1_t);
-    var code = jsfeat.COLOR_RGBA2GRAY;
-    jsfeat.imgproc.grayscale(imageData.data, 640, 480, gray_img, code);
+    var gray_img = new jsfeat.matrix_t(img.width, img.height, jsfeat.U8_t | jsfeat.C1_t);
+    jsfeat.imgproc.grayscale(imageData.data, img.width, img.height, gray_img, jsfeat.COLOR_RGBA2GRAY);
     stat.stop("grayscale");
 
     // render result back to canvas
@@ -47,6 +34,14 @@ function tick() {
 
 document.addEventListener('DOMContentLoaded', function() {
     canvas = document.getElementById('canvas');    
-    demo_app();
+    canvasWidth = canvas.width;
+    canvasHeight = canvas.height;
+    ctx = canvas.getContext('2d');
+
+    ctx.fillStyle = "rgb(0,255,0)";
+    ctx.strokeStyle = "rgb(0,255,0)";
+
+    stat.add("grayscale");
+    
     compatibility.requestAnimationFrame(tick);
 });
