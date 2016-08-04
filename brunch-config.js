ef7b350075e179a3fa9f3aa7c8003e209cc673db
Vlad Ki <vlad@kirillov.im>
2016-08-04 11:38:32 -0700

    brunch-config: force polling until double-reloading is investigated

--- brunch-config.js
+++ brunch-config.js
@@ -4,5 +4,11 @@ module.exports = {
     javascripts: {joinTo: 'app.js'},
     stylesheets: {joinTo: 'app.css'},
     templates: {joinTo: 'app.js'}
+  },
+  watcher: {
+    usePolling: true,
+    // ignored: [/.*#.*/],
+    // atomic: 5000,
+    // awaitWriteFinish: true
   }
 }
